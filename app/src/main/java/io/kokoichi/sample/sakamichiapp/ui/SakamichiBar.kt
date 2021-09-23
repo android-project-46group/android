e671918d75cec18089650cc4b73736dc035852c1
kokoichi206 <tt.300607@gmail.com>
2021-09-24 01:31:12 +0900

    fix: 縦線が被ってた問題を解決

--- app/src/main/java/io/kokoichi/sample/sakamichiapp/ui/SakamichiBar.kt
+++ app/src/main/java/io/kokoichi/sample/sakamichiapp/ui/SakamichiBar.kt
@@ -3,49 +3,71 @@ package io.kokoichi.sample.sakamichiapp.ui
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun GroupList() {
     val groups = mockGroups
     val BORDER_COLOR = Color.Gray
-    val BORDER_RADIUS = 2.dp
-    Row(
+    val BORDER_THICKNESS = 2.dp
+
+    val FONT_SIZE = 20.sp
+    val FONT_COLOR = Color.Black
+
+    // Row の margin が見つからなかったので、外 BOX の padding で対応
+    Box(
         modifier = Modifier
-            .padding(all = 8.dp)
-            .fillMaxWidth()
+            .padding(8.dp),
     ) {
-        Box(
+        Row(
             modifier = Modifier
-                .weight(1f)
-                .border(BorderStroke(BORDER_RADIUS, BORDER_COLOR))
-                .padding(4.dp),
-            contentAlignment = Alignment.Center
+                .border(BorderStroke(BORDER_THICKNESS, BORDER_COLOR))
+                .fillMaxWidth()
+                .height(IntrinsicSize.Min)
         ) {
-            Text(text = "乃木坂")
-        }
-        Box(
-            modifier = Modifier
-                .weight(1f)
-                .border(BorderStroke(BORDER_RADIUS, BORDER_COLOR))
-                .padding (4.dp),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(text = "櫻坂")
-        }
-        Box(
-            modifier = Modifier
-                .weight(1f)
-                .border(BorderStroke(BORDER_RADIUS, BORDER_COLOR))
-                .padding (4.dp),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(text = "日向坂")
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(4.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = "乃木坂", fontSize = FONT_SIZE)
+            }
+            Divider(
+                color = Color.Black,
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .width(BORDER_THICKNESS)
+            )
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(4.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = "櫻坂", fontSize = FONT_SIZE)
+            }
+            Divider(
+                color = Color.Black,
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .width(BORDER_THICKNESS)
+            )
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(4.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = "日向坂", fontSize = FONT_SIZE)
+            }
         }
     }
 }
