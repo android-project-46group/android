07695f9cc575bd515ba211656600484acf20026d
kokoichi206 <tt.300607@gmail.com>
2021-11-05 03:54:14 +0900

    feat: define domain models and repository

---
+++ app/src/main/java/io/kokoichi/sample/sakamichiapp/domain/model/Member.kt
@@ -0,0 +1,14 @@
+package io.kokoichi.sample.sakamichiapp.domain.model
+
+/**
+ * Data class for one member in business logic.
+ */
+data class Member(
+    val blogUrl: String,
+    val bloodType: String,
+    val generation: String,
+    val height: String,
+    val imgUrl: String,
+    val name: String,
+    val birthday: String,
+)
---
+++ app/src/main/java/io/kokoichi/sample/sakamichiapp/domain/repository/SakamichiRepository.kt
@@ -0,0 +1,17 @@
+package io.kokoichi.sample.sakamichiapp.domain.repository
+
+import io.kokoichi.sample.sakamichiapp.data.remote.dto.MembersDto
+import io.kokoichi.sample.sakamichiapp.data.remote.dto.PositionsDto
+import io.kokoichi.sample.sakamichiapp.data.remote.dto.SongsDto
+
+/**
+ * Repository Interface.
+ */
+interface SakamichiRepository {
+
+    suspend fun getMembers(groupName: String): MembersDto
+
+    suspend fun getSongs(groupName: String): SongsDto
+
+    suspend fun getPositions(title: String): PositionsDto
+}
