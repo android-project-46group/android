14ec38ad0b2af4329aaffece737418e6909b3072
kokoichi206 <tt.300607@gmail.com>
2021-11-14 12:29:28 +0900

    test: implemented quiz screen ui test (#27)

---
+++ app/src/androidTest/java/io/kokoichi/sample/sakamichiapp/presentation/quiz/PlayQuizScreenTest.kt
@@ -0,0 +1,122 @@
+package io.kokoichi.sample.sakamichiapp.presentation.quiz
+
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.ui.test.*
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.test.platform.app.InstrumentationRegistry
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
+import io.kokoichi.sample.sakamichiapp.R
+import io.kokoichi.sample.sakamichiapp.di.AppModule
+import io.kokoichi.sample.sakamichiapp.presentation.MainActivity
+import io.kokoichi.sample.sakamichiapp.presentation.util.TestTags
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@ExperimentalMaterialApi
+@ExperimentalCoroutinesApi
+@HiltAndroidTest
+@UninstallModules(AppModule::class)
+class PlayQuizScreenTest {
+
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 1)
+    val composeRule = createAndroidComposeRule<MainActivity>()
+
+    @Before
+    fun setUp() {
+        hiltRule.inject()
+
+        composeRule.setContent {
+            QuizScreen()
+        }
+    }
+
+    @Test
+    fun playPage_displayCorrectParts() {
+        // Arrange
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        val expectedStr = context.resources.getString(
+            R.string.choose_group_name_text,
+            GroupName.values()[0].jname
+        )
+        composeRule
+            .onNodeWithText(expectedStr)
+            .performClick()
+        val quizType = QuizType.values()[1].jname
+        composeRule
+            .onNodeWithText(quizType)
+            .performClick()
+
+        // Act
+
+        // Assert
+        // Check the Logcat (Debug , "TAG")
+        composeRule.onRoot().printToLog("TAG")
+        composeRule
+            .onNodeWithTag(TestTags.PLAY_QUIZ_TITLE)
+            .assertExists()
+        composeRule
+            .onAllNodesWithTag(TestTags.PLAY_QUIZ_ONE_CHOICE)[0]
+            .assertExists()
+        composeRule
+            .onNodeWithTag(TestTags.PLAY_QUIZ_PROGRESS_BAR)
+            .assertExists()
+    }
+
+    @Test
+    fun playPlayBeforeTappingAns_notDisplayCircleOrClose() {
+        // Arrange
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        val expectedStr = context.resources.getString(
+            R.string.choose_group_name_text,
+            GroupName.values()[0].jname
+        )
+        composeRule
+            .onNodeWithText(expectedStr)
+            .performClick()
+        val quizType = QuizType.values()[1].jname
+        composeRule
+            .onNodeWithText(quizType)
+            .performClick()
+
+        // Act
+
+        // Assert
+        composeRule
+            .onNodeWithTag(TestTags.WRONG_CORRECT_ICON)
+            .assertDoesNotExist()
+    }
+
+    @Test
+    fun playPageWhenTapAns_displayCircleOrClose() {
+        // Arrange
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        val expectedStr = context.resources.getString(
+            R.string.choose_group_name_text,
+            GroupName.values()[0].jname
+        )
+        composeRule
+            .onNodeWithText(expectedStr)
+            .performClick()
+        val quizType = QuizType.values()[1].jname
+        composeRule
+            .onNodeWithText(quizType)
+            .performClick()
+
+        // Act
+        composeRule
+            .onAllNodesWithTag(TestTags.PLAY_QUIZ_ONE_CHOICE)[0]
+            .performClick()
+
+        // Assert
+        composeRule
+            .onNodeWithTag(TestTags.WRONG_CORRECT_ICON)
+            .assertExists()
+    }
+}
