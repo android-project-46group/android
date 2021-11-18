c1ae385d80836b6328f1b4f2e28fca1b58daca9e
kokoichi206 <tt.300607@gmail.com>
2021-11-18 23:31:37 +0900

    feat: implemented routings in settings (#32)

---
+++ app/src/main/java/io/kokoichi/sample/sakamichiapp/presentation/setting/ClearCacheScreen.kt
@@ -0,0 +1,9 @@
+package io.kokoichi.sample.sakamichiapp.presentation.setting
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ClearCacheScreen() {
+    Text(text = "clear cache screen")
+}
---
+++ app/src/main/java/io/kokoichi/sample/sakamichiapp/presentation/setting/SettingTopScreen.kt
@@ -0,0 +1,9 @@
+package io.kokoichi.sample.sakamichiapp.presentation.setting
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SettingTopScreen() {
+    Text("設定")
+}
