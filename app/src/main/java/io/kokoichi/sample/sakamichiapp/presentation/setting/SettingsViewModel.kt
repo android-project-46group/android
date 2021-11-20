1d5c02e21a098e33b84c63a873cee8be14c4a3c8
kokoichi206 <tt.300607@gmail.com>
2021-11-21 01:02:00 +0900

    feat: implemented settingsViewModel (#34)

---
+++ app/src/main/java/io/kokoichi/sample/sakamichiapp/presentation/setting/SettingsViewModel.kt
@@ -0,0 +1,50 @@
+package io.kokoichi.sample.sakamichiapp.presentation.setting
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import io.kokoichi.sample.sakamichiapp.domain.usecase.quiz_record.RecordUseCases
+import io.kokoichi.sample.sakamichiapp.presentation.quiz.GroupName
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SettingsViewModel @Inject constructor(
+    private val recordUseCase: RecordUseCases
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(SettingsUiState())
+    var uiState: StateFlow<SettingsUiState> = _uiState
+
+    init {
+        getAccuracy()
+    }
+
+    fun getAccuracy() {
+        if (_uiState.value.records.size != 0) {
+            return
+        }
+        viewModelScope.launch {
+            val groups = GroupName.values()
+            groups.forEach { group ->
+                recordUseCase.getAccuracy(
+                    group = group.name
+                ).let { res ->
+                    _uiState.update {
+                        it.copy(
+                            records = (it.records + Record(
+                                group = group.jname,
+                                correct = res[0],
+                                total = res[1],
+                            )).toMutableList()
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
