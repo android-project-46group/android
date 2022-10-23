afcc73950c5afd8d3c27c6d098340ae5e0c0abeb
takahiro.tominaga <tt.300607@gmail.com>
2022-10-24 01:50:15 +0900

    feat: show skeleton screen when loading in blog screen

---
+++ app/src/main/java/jp/mydns/kokoichi0206/sakamichiapp/presentation/blog/components/SkeletonBlogScreen.kt
@@ -0,0 +1,108 @@
+package jp.mydns.kokoichi0206.sakamichiapp.presentation.blog.components
+
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush.Companion.linearGradient
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+/**
+ * Skeleton Screen
+ * ref: https://mui.com/material-ui/react-skeleton/#variants
+ */
+@Composable
+fun SkeletonBlogScreen() {
+    LazyColumn {
+        items(8) {
+            Row {
+                Box(modifier = Modifier.weight(1f))
+                SkeletonPart(120.dp)
+                Box(modifier = Modifier.weight(1f))
+                SkeletonPart(120.dp)
+                Box(modifier = Modifier.weight(1f))
+                SkeletonPart(120.dp)
+                Box(modifier = Modifier.weight(1f))
+            }
+        }
+    }
+}
+
+@Composable
+fun SkeletonPart(
+    size: Dp,
+) {
+    val gradient = listOf(
+        Color.LightGray.copy(alpha = 0.9f),
+        Color.LightGray.copy(alpha = 0.3f),
+        Color.LightGray.copy(alpha = 0.9f)
+    )
+
+    val transition = rememberInfiniteTransition()
+
+    val translateAnimation = transition.animateFloat(
+        initialValue = 0f,
+        targetValue = 1000f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(
+                durationMillis = 1000,
+                easing = FastOutLinearInEasing
+            )
+        )
+    )
+    val brush = linearGradient(
+        colors = gradient,
+        start = Offset(200f, 200f),
+        end = Offset(
+            x = translateAnimation.value,
+            y = translateAnimation.value
+        )
+    )
+    Column(
+        modifier = Modifier
+            .padding(vertical = 8.dp)
+            .width(size)
+    ) {
+        Spacer(
+            modifier = Modifier
+                .size(120.dp)
+                .padding(2.dp)
+                .clip(MaterialTheme.shapes.medium)
+                .background(brush)
+        )
+        Spacer(
+            modifier = Modifier
+                .height(8.dp)
+        )
+        Spacer(
+            modifier = Modifier
+                .height(20.dp)
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(10.dp))
+                .background(brush)
+        )
+        Spacer(
+            modifier = Modifier
+                .height(12.dp)
+        )
+        Spacer(
+            modifier = Modifier
+                .height(20.dp)
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(10.dp))
+                .background(brush)
+        )
+        Spacer(
+            modifier = Modifier
+                .height(12.dp)
+        )
+    }
+}
