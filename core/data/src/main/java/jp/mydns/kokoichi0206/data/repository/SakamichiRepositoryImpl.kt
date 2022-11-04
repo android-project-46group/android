b7b3f2a66a94918c771956d55c39118192b180d5
takahiro.tominaga <tt.300607@gmail.com>
2022-11-05 05:51:51 +0900

    fix: remove comment out (#83)

--- core/data/src/main/java/jp/mydns/kokoichi0206/data/repository/SakamichiRepositoryImpl.kt
+++ core/data/src/main/java/jp/mydns/kokoichi0206/data/repository/SakamichiRepositoryImpl.kt
@@ -15,27 +15,22 @@ class SakamichiRepositoryImpl @Inject constructor(
 
     override suspend fun getMembers(groupName: String): MembersDto {
         return api.getMembers(groupName, buildConfig.API_KEY)
-//        return api.getMembers(groupName, "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855")
     }
 
     override suspend fun getBlogs(groupName: String): BlogsDto {
         return api.getBlogs(groupName, buildConfig.API_KEY)
-//        return api.getBlogs(groupName, "buildConfig.API_KEY")
     }
 
     override suspend fun getSongs(groupName: String): SongsDto {
         return api.getSongs(groupName, buildConfig.API_KEY)
-//        return api.getSongs(groupName, "buildConfig.API_KEY")
     }
 
     override suspend fun getPositions(title: String): PositionsDto {
         return api.getPositions(title, buildConfig.API_KEY)
-//        return api.getPositions(title, "buildConfig.API_KEY")
     }
 
     override suspend fun updateBlog(): UpdateBlogResponseDto {
         return api.updateBlog(buildConfig.API_KEY)
-//        return api.updateBlog("buildConfig.API_KEY")
     }
 
     override suspend fun reportIssue(message: String): ReportIssueResponseDto {
