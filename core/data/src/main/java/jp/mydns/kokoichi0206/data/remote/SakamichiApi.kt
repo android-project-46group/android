a156fe1d2a3a311772729b270ad4cf744e526852
takahiro.tominaga <tt.300607@gmail.com>
2022-11-05 05:53:08 +0900

    refine: set default value for api key

--- core/data/src/main/java/jp/mydns/kokoichi0206/data/remote/SakamichiApi.kt
+++ core/data/src/main/java/jp/mydns/kokoichi0206/data/remote/SakamichiApi.kt
@@ -14,31 +14,31 @@ interface SakamichiApi {
     @GET("api/v1/members")
     suspend fun getMembers(
         @Query("gn") groupName: String,
-        @Query("key") apiKey: String,
+        @Query("key") apiKey: String = "",
     ): MembersDto
 
     @GET("api/v1/blogs")
     suspend fun getBlogs(
         @Query("gn") groupName: String,
-        @Query("key") apiKey: String,
+        @Query("key") apiKey: String = "",
     ): BlogsDto
 
     @GET("api/v1/songs")
     suspend fun getSongs(
         @Query("gn") groupName: String,
-        @Query("key") apiKey: String,
+        @Query("key") apiKey: String = "",
     ): SongsDto
 
     @GET("api/v1/positions")
     suspend fun getPositions(
         @Query("title") title: String,
-        @Query("key") apiKey: String,
+        @Query("key") apiKey: String = "",
     ): PositionsDto
 
     // This GET method updated blog info in db.
     @GET("blog/update/")
     suspend fun updateBlog(
-        @Query("key") apiKey: String,
+        @Query("key") apiKey: String = "",
     ): UpdateBlogResponseDto
 
     // This POST method send messages to server.
